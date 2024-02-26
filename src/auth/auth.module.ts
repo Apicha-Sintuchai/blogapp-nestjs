8aff350dfc8c4484a6c4d8447ceb7d34412604e8
Apicha-Sintuchai <apichai2556anet@gmail.com>
2024-02-26 20:59:20 +0700

    dadwaddwa

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
--- src/blog-app/blog-app.module.ts
+++ src/blog-app/blog-app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { BlogAppController } from './blog-app.controller';
 import { BlogAppService } from './blog-app.service';
